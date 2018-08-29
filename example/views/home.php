69fe831e862cc23bfc84fadba70f9e3f5e782b4e
Paolo Greppi <paolo.greppi@libpf.com>
2018-08-29 16:40:41 +0200

    IdP selection on login
    while we wait for spid-smart-button, the list of IdPs is hardwired

--- example/views/home.php
+++ example/views/home.php
@@ -1,4 +1,12 @@
 <?php
-echo '<a href="/login">Login</a><br>';
+echo '<a href="/login?idp=testenv">Login with testenv</a><br>';
+echo '<a href="/login?idp=idp_1">Login with https://identity.infocert.it</a><br>';
+echo '<a href="/login?idp=idp_2">Login with https://posteid.poste.it</a><br>';
+echo '<a href="/login?idp=idp_3">Login with https://login.id.tim.it/affwebservices/public/saml2sso</a><br>';
+echo '<a href="/login?idp=idp_4">Login with https://identity.sieltecloud.it</a><br>';
+echo '<a href="/login?idp=idp_5">Login with https://loginspid.aruba.it</a><br>';
+echo '<a href="/login?idp=idp_6">Login with https://idp.namirialtsp.com/idp</a><br>';
+echo '<a href="/login?idp=idp_7">Login with https://spid.register.it</a><br>';
+echo '<a href="/login?idp=idp_8">Login with https://spid.intesa.it</a><br>';
 echo '<a href="/acs">Acs</a><br>';
 echo '<a href="/metadata">Metadata</a><br>';
--- example/views/login.php
+++ example/views/login.php
@@ -1,6 +1,12 @@
 <?php
 
-if (!$sp->login("testenv", 0, 1)) {
-    echo "Already logged in !<br>";
-    echo "<a href=\"/\">Home</a>";
+if (empty($_GET) or !isset($_GET['idp'])) {
+  // TODO report error
+  echo "Missing idp parameter<br>";
+} else {
+    $idp = $_GET['idp'];
+    if (!$sp->login($idp, 0, 1)) {
+        echo "Already logged in !<br>";
+    }
 }
+echo "<a href=\"/\">Home</a>";
